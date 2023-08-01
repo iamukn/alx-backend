84f4124012b42ad6fe0d012a4ab70c294499c7c5
iamukn <n.u.kingsley@gmail.com>
2023-08-01 22:28:36 +0000

    babel.localeselector usage

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+""" A simple flask app that serves an html page """
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+class Config(object):
+    """languages"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """gets the locale of
+    user
+    """
+
+    return request.accept_languages.best_match(app.config(['LANGUAGES']))
+
+
+@app.route('/')
+def home():
+    """ Home route that serves the
+    landing page """
+
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host="0.0.0.0", debug=True)
